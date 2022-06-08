ab3ab23b7086964b31302b49a09daf54a1f2eaad
Jonathan Howard <jonathanhoward@fastmail.com>
2022-06-08 11:06:32 -0400

    Refactor calculate

--- src/lib/calculate.ts
+++ src/lib/calculate.ts
@@ -5,13 +5,10 @@ class CalculatorError extends Error {
   }
 }
 
+let tokens: string[];
+
 export function calculate(expressionString: string): string {
-  const mantissa = "-?\\d+(?:\\.\\d*)?";
-  const exponent = "e[-+]\\d+";
-  const number = `${mantissa}(?:${exponent})?`;
-  const operator = "[+−×÷=]";
-  const numberOrOperator = new RegExp(`${number}|${operator}`, "g");
-  const tokens = expressionString.match(numberOrOperator) ?? [];
+  tokens = parseTokens(expressionString);
 
   let result: string;
 
@@ -26,74 +23,16 @@ export function calculate(expressionString: string): string {
   }
 
   return result;
+}
 
-  /**
-   * The following code is taken from:
-   *
-   *     Stroustrup, Bjarne. "Programming: Principles and Practice Using
-   *     C++." 2nd ed., Addison-Wesley Professional, 2014.
-   *
-   * It was translated into Javascript, modified to use an Array<Token>
-   * instead of a Token_stream, and removed unused features.
-   */
+function parseTokens(tokenString: string): string[] {
+  const mantissa = "-?\\d+(?:\\.\\d*)?";
+  const exponent = "e[-+]\\d+";
+  const number = `${mantissa}(?:${exponent})?`;
+  const operator = "[+−×÷=]";
+  const numberOrOperator = new RegExp(`${number}|${operator}`, "g");
 
-  function expression(): number {
-    let left = term();
-    let token = tokens.shift();
-
-    while (true) {
-      switch (token) {
-        case "+":
-          left += term();
-          token = tokens.shift();
-          break;
-        case "−":
-          left -= term();
-          token = tokens.shift();
-          break;
-        case "=":
-          return left;
-        default:
-          throw new CalculatorError("expression error");
-      }
-    }
-  }
-
-  function term(): number {
-    let left = primary();
-    let token = tokens.shift();
-
-    while (true) {
-      switch (token) {
-        case "×":
-          left *= primary();
-          token = tokens.shift();
-          break;
-        case "÷":
-          const d = primary();
-          if (d === 0) {
-            throw new CalculatorError("divide by zero");
-          }
-          left /= d;
-          token = tokens.shift();
-          break;
-        default:
-          if (token) tokens.unshift(token);
-          return left;
-      }
-    }
-  }
-
-  function primary(): number {
-    const token = tokens.shift();
-
-    const result = Number(token);
-    if (Number.isNaN(result)) {
-      throw new CalculatorError("primary expected");
-    }
-
-    return result;
-  }
+  return tokenString.match(numberOrOperator) ?? [];
 }
 
 function setPrecision10(n: number): string {
@@ -113,4 +52,72 @@ function setPrecision10(n: number): string {
   return absNumber < 1e-6 || 1e10 <= absNumber
     ? Number(precision10).toExponential()
     : Number(precision10).toString();
+}
+
+/**
+ * The following code is taken from:
+ *
+ *     Stroustrup, Bjarne. "Programming: Principles and Practice Using
+ *     C++." 2nd ed., Addison-Wesley Professional, 2014.
+ *
+ * It was translated into Javascript, modified to use an Array<Token>
+ * instead of a Token_stream, and removed unused features.
+ */
+
+function expression(): number {
+  let left = term();
+  let token = tokens.shift();
+
+  while (true) {
+    switch (token) {
+      case "+":
+        left += term();
+        token = tokens.shift();
+        break;
+      case "−":
+        left -= term();
+        token = tokens.shift();
+        break;
+      case "=":
+        return left;
+      default:
+        throw new CalculatorError("expression error");
+    }
+  }
+}
+
+function term(): number {
+  let left = primary();
+  let token = tokens.shift();
+
+  while (true) {
+    switch (token) {
+      case "×":
+        left *= primary();
+        token = tokens.shift();
+        break;
+      case "÷":
+        const d = primary();
+        if (d === 0) {
+          throw new CalculatorError("divide by zero");
+        }
+        left /= d;
+        token = tokens.shift();
+        break;
+      default:
+        if (token) tokens.unshift(token);
+        return left;
+    }
+  }
+}
+
+function primary(): number {
+  const token = tokens.shift();
+
+  const result = Number(token);
+  if (Number.isNaN(result)) {
+    throw new CalculatorError("primary expected");
+  }
+
+  return result;
 }

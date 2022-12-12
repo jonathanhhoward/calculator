84a6b13885705676fea9c74038a3a54016971f79
Jonathan Howard <jonathanhoward@fastmail.com>
2022-12-12 10:46:09 -0500

    Refactored initial state

--- src/app/state/state.service.provider.ts
+++ src/app/state/state.service.provider.ts
@@ -1,22 +1,22 @@
-import { InjectionToken } from "@angular/core";
+import { Injectable, InjectionToken } from "@angular/core";
 import { AppReducer } from "app/state/app-reducer";
 import { AppState } from "app/state/app-state";
 import { InputReducer } from "app/state/input.reducer";
 import { StateService } from "app/state/state.service";
 
-export const APP_STATE = new InjectionToken("app-state", {
-  providedIn: "root",
-  factory: () => ({
-    expression: "",
-    input: "0",
-  }),
-});
+export const APP_STATE = new InjectionToken("AppState");
 
-export const APP_REDUCER = new InjectionToken("app-reducer");
+export const APP_REDUCER = new InjectionToken("AppReducer");
+
+@Injectable({ providedIn: "root" })
+class InitialState implements AppState {
+  expression = "";
+  input = "0";
+}
 
 export const stateServiceProvider = {
   provide: StateService,
-  useFactory: (state: AppState, reducer: AppReducer) =>
-    new StateService(state, reducer),
-  deps: [APP_STATE, InputReducer],
+  useFactory: (initialState: AppState, reducer: AppReducer) =>
+    new StateService(initialState, reducer),
+  deps: [InitialState, InputReducer],
 };

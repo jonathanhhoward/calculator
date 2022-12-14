d2299f3ea5a317a2eda21eb4ebb40feaabb081c1
Jonathan Howard <jonathanhoward@fastmail.com>
2022-12-13 20:47:00 -0500

    Removed @angular-builders/jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  moduleNameMapper: {
-    "@core/(.*)": "<rootDir>/src/app/core/$1",
-  },
   moduleDirectories: ["node_modules", "<rootDir>/src"],
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],

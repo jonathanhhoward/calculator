01bcd56270b11da36771b2378b4ac4f1b793c19d
Jonathan Howard <jonathanhoward@fastmail.com>
2022-12-17 16:24:38 -0500

    Moved negate click tests to file

---
+++ src/app/__tests__/negate-click.ts
@@ -0,0 +1,51 @@
+import {
+  expectDisplayTextContent,
+  fireClickEvents,
+} from "app/test-utils/helpers";
+import renderApp from "app/test-utils/render-app";
+
+test("prepends current input with '-' if positive", async () => {
+  const { display, keyPad } = await renderApp();
+  const { add, negate, one } = keyPad;
+
+  fireClickEvents([one, negate]);
+  expectDisplayTextContent(display, "", "-1");
+
+  fireClickEvents([add, one, negate]);
+  expectDisplayTextContent(display, "-1+", "-1");
+});
+
+test("removes '-' from current input if negative", async () => {
+  const { display, keyPad } = await renderApp();
+  const { add, negate, one } = keyPad;
+
+  fireClickEvents([one, negate, negate]);
+  expectDisplayTextContent(display, "", "1");
+
+  fireClickEvents([add, one, negate, negate]);
+  expectDisplayTextContent(display, "1+", "1");
+});
+
+test("does not negate zero", async () => {
+  const { display, keyPad } = await renderApp();
+  const { negate } = keyPad;
+
+  fireClickEvents([negate]);
+  expectDisplayTextContent(display, "", "0");
+});
+
+test("does nothing after operator", async () => {
+  const { display, keyPad } = await renderApp();
+  const { add, negate } = keyPad;
+
+  fireClickEvents([add, negate]);
+  expectDisplayTextContent(display, "0", "+");
+});
+
+test("keeps expression negates result", async () => {
+  const { display, keyPad } = await renderApp();
+  const { negate, equals, one } = keyPad;
+
+  fireClickEvents([one, equals, negate]);
+  expectDisplayTextContent(display, "1=", "-1");
+});
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,60 +1,11 @@
 import "@testing-library/jest-dom/extend-expect";
 import {
   expectDisplayTextContent,
-  fireClickEvents,
   fireKeydownEvents,
 } from "app/test-utils/helpers";
 import renderApp from "app/test-utils/render-app";
 
 describe("display on key click", () => {
-  describe("negate", () => {
-    test("prepends current input with '-' if positive", async () => {
-      const { display, keyPad } = await renderApp();
-      const { add, negate, one } = keyPad;
-
-      fireClickEvents([one, negate]);
-      expectDisplayTextContent(display, "", "-1");
-
-      fireClickEvents([add, one, negate]);
-      expectDisplayTextContent(display, "-1+", "-1");
-    });
-
-    test("removes '-' from current input if negative", async () => {
-      const { display, keyPad } = await renderApp();
-      const { add, negate, one } = keyPad;
-
-      fireClickEvents([one, negate, negate]);
-      expectDisplayTextContent(display, "", "1");
-
-      fireClickEvents([add, one, negate, negate]);
-      expectDisplayTextContent(display, "1+", "1");
-    });
-
-    test("does not negate zero", async () => {
-      const { display, keyPad } = await renderApp();
-      const { negate } = keyPad;
-
-      fireClickEvents([negate]);
-      expectDisplayTextContent(display, "", "0");
-    });
-
-    test("does nothing after operator", async () => {
-      const { display, keyPad } = await renderApp();
-      const { add, negate } = keyPad;
-
-      fireClickEvents([add, negate]);
-      expectDisplayTextContent(display, "0", "+");
-    });
-
-    test("keeps expression negates result", async () => {
-      const { display, keyPad } = await renderApp();
-      const { negate, equals, one } = keyPad;
-
-      fireClickEvents([one, equals, negate]);
-      expectDisplayTextContent(display, "1=", "-1");
-    });
-  });
-
   describe("keyboard", () => {
     test("handles numpad keydown events", async () => {
       const { display } = await renderApp();

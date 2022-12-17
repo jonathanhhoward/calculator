d52dca70f779780d600832c4c6f3d74c28c9bd9c
Jonathan Howard <jonathanhoward@fastmail.com>
2022-12-17 16:15:00 -0500

    Moved delete click tests to file

---
+++ src/app/__tests__/delete-click.ts
@@ -0,0 +1,35 @@
+import {
+  expectDisplayTextContent,
+  fireClickEvents,
+} from "app/test-utils/helpers";
+import renderApp from "app/test-utils/render-app";
+
+test("does nothing after operator", async () => {
+  const { display, keyPad } = await renderApp();
+  const { del, add, one } = keyPad;
+
+  fireClickEvents([one, add, del]);
+  expectDisplayTextContent(display, "1", "+");
+});
+
+test("does nothing after result", async () => {
+  const { display, keyPad } = await renderApp();
+  const { del, equals, one } = keyPad;
+
+  fireClickEvents([one, equals, del]);
+  expectDisplayTextContent(display, "1=", "1");
+});
+
+test("overwrites current number input with zero", async () => {
+  const { display, keyPad } = await renderApp();
+  const { del, add, negate, one } = keyPad;
+
+  fireClickEvents([one, del]);
+  expectDisplayTextContent(display, "", "0");
+
+  fireClickEvents([add, one, del]);
+  expectDisplayTextContent(display, "0+", "0");
+
+  fireClickEvents([add, one, negate, del]);
+  expectDisplayTextContent(display, "0+0+", "0");
+});
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,38 +7,6 @@ import {
 import renderApp from "app/test-utils/render-app";
 
 describe("display on key click", () => {
-  describe("delete", () => {
-    test("does nothing after operator", async () => {
-      const { display, keyPad } = await renderApp();
-      const { del, add, one } = keyPad;
-
-      fireClickEvents([one, add, del]);
-      expectDisplayTextContent(display, "1", "+");
-    });
-
-    test("does nothing after result", async () => {
-      const { display, keyPad } = await renderApp();
-      const { del, equals, one } = keyPad;
-
-      fireClickEvents([one, equals, del]);
-      expectDisplayTextContent(display, "1=", "1");
-    });
-
-    test("overwrites current number input with zero", async () => {
-      const { display, keyPad } = await renderApp();
-      const { del, add, negate, one } = keyPad;
-
-      fireClickEvents([one, del]);
-      expectDisplayTextContent(display, "", "0");
-
-      fireClickEvents([add, one, del]);
-      expectDisplayTextContent(display, "0+", "0");
-
-      fireClickEvents([add, one, negate, del]);
-      expectDisplayTextContent(display, "0+0+", "0");
-    });
-  });
-
   describe("equals", () => {
     test("ignored while result displayed", async () => {
       const { display, keyPad } = await renderApp();

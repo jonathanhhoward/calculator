6020521d28f84c82684a9dc0312c1f2517bce3af
Jonathan Howard <jonathanhoward@fastmail.com>
2022-12-17 16:28:47 -0500

    Moved keydown tests to file

---
+++ src/app/__tests__/key-down.ts
@@ -0,0 +1,29 @@
+import {
+  expectDisplayTextContent,
+  fireKeydownEvents,
+} from "app/test-utils/helpers";
+import renderApp from "app/test-utils/render-app";
+
+test("handles numpad keydown events", async () => {
+  const { display } = await renderApp();
+  fireKeydownEvents([
+    "Numpad1",
+    "NumpadDecimal",
+    "Numpad0",
+    "NumpadAdd",
+    "Numpad2",
+    "Numpad3",
+    "NumpadSubtract",
+    "Numpad4",
+    "Numpad5",
+    "NumpadMultiply",
+    "Numpad6",
+    "Numpad7",
+    "NumpadDivide",
+    "Numpad8",
+    "Numpad9",
+    "NumpadEnter",
+  ]);
+
+  expectDisplayTextContent(display, "1.0+23−45×67÷89=", "-9.876404494");
+});

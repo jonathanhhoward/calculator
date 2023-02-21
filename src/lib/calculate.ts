0a80975e8d1cdd08e5ae42b5f060a5f58a98f8e5
Jonathan Howard <jonathanhoward@fastmail.com>
2023-02-20 21:16:38 -0500

    Refactored result formatting

--- src/lib/calculate.ts
+++ src/lib/calculate.ts
@@ -13,20 +13,18 @@ function normalizeOperators(expression: string) {
 }
 
 function setPrecision10(n: number): string {
-  if (Number.isNaN(n)) return NaN.toString();
+  if (isNaN(n)) return NaN.toString();
 
   if (n === 0) return "0";
 
   const absNumber = Math.abs(n);
 
-  if (1e-6 <= absNumber && absNumber < 1) {
-    const round9 = Math.round(n * 1e9) / 1e9;
-    return round9.toString();
-  }
-
-  const precision10 = Number(n).toPrecision(10);
+  const precision10 =
+    1e-6 <= absNumber && absNumber < 1
+      ? Number(n.toFixed(9))
+      : Number(n.toPrecision(10));
 
   return absNumber < 1e-6 || 1e10 <= absNumber
-    ? Number(precision10).toExponential()
-    : Number(precision10).toString();
+    ? precision10.toExponential()
+    : precision10.toString();
 }

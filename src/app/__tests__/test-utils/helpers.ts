9a99d907a3eed413db49a60681b66b65c90e49bc
Jonathan Howard <jonathan.howard@insight.com>
2023-03-21 15:34:40 -0400

    Removed obsolete code

--- src/app/__tests__/test-utils/helpers.ts
+++ src/app/__tests__/test-utils/helpers.ts
@@ -9,17 +9,6 @@ export function fireKeydownEvents(codes: string[]) {
   codes.forEach((code) => fireEvent.keyDown(document.body, { code }));
 }
 
-export function expectDisplayTextContent(
-  display: Display,
-  expectedExpression: string,
-  expectedInput: string
-) {
-  const { expression, input } = display;
-
-  expect(expression.textContent).toBe(expectedExpression);
-  expect(input.textContent).toBe(expectedInput);
-}
-
 export function getTextContent(display: Display): DisplayTextContent {
   return {
     expression: display.expression.textContent,

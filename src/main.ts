a8a535663c4824b7d1f3ff307399b666f5dad64a
Jonathan Howard <Jonathan.Howard@insight.com>
2024-12-31 13:43:41 -0500

    Angular 19 upgrade

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
-import { enableProdMode, importProvidersFrom } from "@angular/core";
-import { bootstrapApplication, BrowserModule } from "@angular/platform-browser";
+import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "app/app.component";
-import { environment } from "environments/environment";
+import { appConfig } from "./app/app.config";
 
-if (environment.production) {
-  enableProdMode();
-}
-
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule)],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);

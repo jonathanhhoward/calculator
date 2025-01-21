a92a5bdc8ca0ef1b5f5b28181b9e8448c0b4e9be
Jonathan Howard <Jonathan.Howard@insight.com>
2025-01-21 14:24:49 -0500

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
+const presets = require("jest-preset-angular/presets");
+
 module.exports = {
+  ...presets.createCjsPreset(),
   moduleDirectories: ["node_modules", "<rootDir>/src"],
-  preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   testPathIgnorePatterns: ["/__tests__/test-utils/"],
 };
